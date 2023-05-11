442c497cea0f0bc838a9725b6cad5ae6025298e7
Eugenia Valls <e.vallsalfaro@gmail.com>
2023-05-11 17:31:52 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import { ViteMinifyPlugin } from "vite-plugin-minify";
 
 export default defineConfig({
   plugins: [ViteMinifyPlugin({}), ViteImageOptimizer({})],
-  base: '',
-  root: 'src',
+  base: "",
+  root: "src",
   build: {
-    outDir: '../dist'
-  }
+    outDir: "../docs",
+  },
 });
